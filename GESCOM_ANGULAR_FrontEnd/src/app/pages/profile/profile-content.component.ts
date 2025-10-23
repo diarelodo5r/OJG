deb0da608bd0a5c9d403525b8d69431335ae638f
CharlyEKLU <charlyeklu@gmail.com>
2025-10-23 13:40:59 +0000

    2nd commit for laravel

--- GESCOM_ANGULAR_FrontEnd/src/app/pages/profile/profile-content.component.ts
+++ GESCOM_ANGULAR_FrontEnd/src/app/pages/profile/profile-content.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../../material.module';
-import { TablerIconsModule } from 'angular-tabler-icons';
+import { NgIcon } from '@ng-icons/core';
 
 @Component({
   selector: 'app-profile-content',
   standalone: true,
-  imports: [CommonModule, MaterialModule, TablerIconsModule],
+  imports: [CommonModule, MaterialModule, NgIcon],
   templateUrl: './profile-content.component.html',
   styleUrl: './profile-content.component.scss'
 })
